a0ebfa9b8a5727dab2dd93b9511f031e4dade070
Danny <danny.goyer177@gmail.com>
2020-02-20 19:14:17 -0500

    Updated myproject.py to use tostring()

--- myproject.py
+++ myproject.py
@@ -11,18 +11,12 @@ print(f'------------------------------------------------------------\n\n')
 print(f'Print descriptive statistics. There are two many columns to fit on the screen so will temporarily remove\n'
       f'limits to pandas display settings.\n')
 
-# Reset limits in pandas.options.display so that all data prints to the console output window
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
-
 # Actual Printing
-print(df.describe().dropna(how='all'))
+print(df.describe().dropna(how='all').to_string())
 print(f'------------------------------------------------------------\n\n')
 
 # Print correlations
 print(f'Printing the correlations. There are two many columns to fit on the screen so will temporarily remove limits\n'
       f'to pandas display settings.\n')
-print(df.corr().dropna(how='all'))
+print(df.corr().dropna(how='all').to_string())
 print(f'------------------------------------------------------------\n\n')
