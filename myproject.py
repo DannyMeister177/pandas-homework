62d5e47c584a77d54419349034dddaaa847bf99d
Danny <danny.goyer177@gmail.com>
2020-02-20 19:15:19 -0500

    Update myproject.py

--- myproject.py
+++ myproject.py
@@ -10,8 +10,6 @@ print(df.info())
 print(f'------------------------------------------------------------\n\n')
 print(f'Print descriptive statistics. There are two many columns to fit on the screen so will temporarily remove\n'
       f'limits to pandas display settings.\n')
-
-# Actual Printing
 print(df.describe().dropna(how='all').to_string())
 print(f'------------------------------------------------------------\n\n')
 
